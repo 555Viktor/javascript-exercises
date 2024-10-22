2f9b185b6f2d419c43025249229d065497fd3e3d
555Viktor <victor.vaskov2002@gmail.com>
2024-10-22 13:54:29 +0300

    Solve exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const getTheTitles = function (obj) {
+    let result = [];
 
+    for (let book of obj) {
+        if (book.title && !result.includes(book.title)) {
+            result.push(book.title);
+        }
+    }
+    
+    return result;
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
