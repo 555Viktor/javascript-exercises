8790fabfe0d289823dabc92e93f6af83a2dd2418
555Viktor <victor.vaskov2002@gmail.com>
2024-10-22 13:15:38 +0300

    Solve 08-calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,24 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 // Do not edit below this line
@@ -28,6 +27,5 @@ module.exports = {
   subtract,
   sum,
   multiply,
-  power,
-  factorial
+  power
 };
