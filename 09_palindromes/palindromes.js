0582e5a3d13f9cbcb5f65c4c14275b3983e42b31
555Viktor <victor.vaskov2002@gmail.com>
2024-10-22 13:44:13 +0300

    Solve exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strCharsArray = str.toLowerCase().split('').filter(char => /^[a-zA-Z]$/.test(char));
+    let strCharsArrayReversed = str.toLowerCase().split('').filter(char => /^[a-zA-Z]$/.test(char)).reverse();
+    
+    return strCharsArray.join('') === strCharsArrayReversed.join('');
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 
 // Do not edit below this line
 module.exports = palindromes;
